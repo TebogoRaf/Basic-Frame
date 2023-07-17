2f613ee4b4d60ee71a1755a1f5fe4980a533bcea
Tebogo Rafapa <121289022+TebogoRaf@users.noreply.github.com>
2023-07-17 17:39:28 +0200

    Add files via upload

---
+++ BasicFrame/src/za/ac/tut/ui/MyFirstFrame.java
@@ -0,0 +1,18 @@
+
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class MyFirstFrame extends JFrame{
+    public MyFirstFrame(){
+        setTitle("MyFirstGUI");
+        setSize(250,300);
+       
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.pink);
+       
+    }
+
+    
+}
