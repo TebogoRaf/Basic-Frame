4cd5e8a9ed166ed6a56ae779798410aace109492
Tebogo Rafapa <121289022+TebogoRaf@users.noreply.github.com>
2023-07-21 22:41:29 +0200

    Update MyFirstFrame.java

--- BasicFrame/src/za/ac/tut/ui/MyFirstFrame.java
+++ BasicFrame/src/za/ac/tut/ui/MyFirstFrame.java
@@ -6,12 +6,13 @@ import javax.swing.JFrame;
 
 public class MyFirstFrame extends JFrame{
     public MyFirstFrame(){
-        setTitle("MyFirstGUI");
-        setSize(250,300);
-       
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+       setTitle("MY FIRST GUI");
+        setSize(200,250);
+        setDefaultLookAndFeelDecorated(true);
         setForeground(Color.pink);
-       
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
     
